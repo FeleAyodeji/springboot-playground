c1960f9c41f97ab6fbcb69f4fb986d2235337331
FeleAyodeji <feledeji@gmail.com>
2025-08-07 00:34:21 +0100

    fundamentals of rest api

--- springboot-rest-api/springboot-rest-api/src/main/java/net/javaguides/springboot_rest_api/Bean/Student.java
+++ springboot-rest-api/springboot-rest-api/src/main/java/net/javaguides/springboot_rest_api/Bean/Student.java
@@ -1,4 +1,37 @@
 package net.javaguides.springboot_rest_api.Bean;
 
 public class Student {
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public Student(String firstName, String lastName, int id) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
--- springboot-rest-api/springboot-rest-api/src/main/java/net/javaguides/springboot_rest_api/Controller/HelloWorldController.java
+++ springboot-rest-api/springboot-rest-api/src/main/java/net/javaguides/springboot_rest_api/Controller/HelloWorldController.java
@@ -1,4 +1,13 @@
 package net.javaguides.springboot_rest_api.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorldController {
+
+     @GetMapping("/hello-world")
+    public String HelloWorld() {
+        return "Hello World";
+    }
 }

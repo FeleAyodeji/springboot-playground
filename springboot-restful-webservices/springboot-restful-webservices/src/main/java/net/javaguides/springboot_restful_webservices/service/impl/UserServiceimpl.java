bde919b699057b4a67647e5711a758bfb1ed1930
FeleAyodeji <feledeji@gmail.com>
2025-08-17 14:40:32 +0100

    Build create USER rest api

---
+++ springboot-restful-webservices/springboot-restful-webservices/src/main/java/net/javaguides/springboot_restful_webservices/service/impl/UserServiceimpl.java
@@ -0,0 +1,19 @@
+package net.javaguides.springboot_restful_webservices.service.impl;
+
+import lombok.AllArgsConstructor;
+import net.javaguides.springboot_restful_webservices.entity.User;
+import net.javaguides.springboot_restful_webservices.repository.UserRepository;
+import net.javaguides.springboot_restful_webservices.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserServiceimpl implements UserService {
+
+    private UserRepository userRepository;
+
+    @Override
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+}

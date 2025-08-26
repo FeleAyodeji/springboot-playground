0a4a15f0a136e2d6d3a635a83278e8e1be682148
FeleAyodeji <feledeji@gmail.com>
2025-08-26 20:29:29 +0100

    feat: add Swagger API documentation with Springdoc OpenAPI

--- springboot-exception-handling/springboot-exception-handling/src/main/java/net/javaguides/springboot_exception_handling/dto/UserDto.java
+++ springboot-exception-handling/springboot-exception-handling/src/main/java/net/javaguides/springboot_exception_handling/dto/UserDto.java
@@ -1,5 +1,6 @@
 package net.javaguides.springboot_exception_handling.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
@@ -15,13 +16,23 @@ public class UserDto {
 
     private Long id;
     // User first name should not be null or empty
+
+    @Schema(
+            description = "User First Name"
+    )
     @NotEmpty(message = "User first name should not be null or empty")
     private String firstName;
 
+    @Schema(
+            description = "User Last Name"
+    )
     // User last name should not be null or empty
     @NotEmpty(message = " User last name should not be null or empty")
     private String lastName;
 
+    @Schema(
+            description = "User Email Address"
+    )
     // User email should not be null or empty
     // Email address should be valid
     @NotEmpty(message = "User email should not be null or empty")
